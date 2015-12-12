4afe00a97a5ab166e31678594422b46b2de1b7aa
Alex-Besson <alex_j67@hotmail.com>
2015-12-12 08:56:52 -0500

    Initial commit. Added the basic layout and created the monster anymation.

---
+++ My Little Monster/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  My Little Monster
+//
+//  Created by Alexander Besson on 2015-12-12.
+//  Copyright © 2015 Alexander Besson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgMonster: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imgMonster.animationImages = []
+        
+        var imgArray = [UIImage]()
+        
+        for var x = 1; x <= 4; x++ {
+            var img = UIImage(named: "idle\(x)")
+            imgArray.append(img!)
+        }
+        
+        imgMonster.animationImages = imgArray
+        imgMonster.animationDuration = 0.8
+        imgMonster.animationRepeatCount = 0
+        imgMonster.startAnimating()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
