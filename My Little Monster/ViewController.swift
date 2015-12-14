7eafda45f5b27e07a60a120d5ee5bcb3660969aa
Alex-Besson <alex_j67@hotmail.com>
2015-12-14 16:17:25 -0500

    Monster lives and dies.

--- My Little Monster/ViewController.swift
+++ My Little Monster/ViewController.swift
@@ -10,30 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var imgMonster: UIImageView!
+    @IBOutlet weak var imgMonster: MonsterImage!
     @IBOutlet weak var imgFood: DragImage!
     @IBOutlet weak var imgHeart: DragImage!
 
+    @IBOutlet weak var imgLeftSkull: UIImageView!
+    @IBOutlet weak var imgCenterSkull: UIImageView!
+    @IBOutlet weak var imgRightSkull: UIImageView!
+    
+    let DIM_ALPHA: CGFloat = 0.2
+    let OPAQUE_ALPHA: CGFloat = 1.0
+    let MAX_PENALTIES = 3
+    var penalties = 0
+    
+    var timer: NSTimer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        imgMonster.animationImages = []
+        imgFood.dropTarget = imgMonster
+        imgHeart.dropTarget = imgMonster
         
-        var imgArray = [UIImage]()
+        imgLeftSkull.alpha = DIM_ALPHA
+        imgCenterSkull.alpha = DIM_ALPHA
+        imgRightSkull.alpha = DIM_ALPHA
         
-        for var x = 1; x <= 4; x++ {
-            var img = UIImage(named: "idle\(x)")
-            imgArray.append(img!)
-        }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "itemDroppedOnCharacter:", name: "onTargetDropped", object: nil)
         
-        imgMonster.animationImages = imgArray
-        imgMonster.animationDuration = 0.8
-        imgMonster.animationRepeatCount = 0
-        imgMonster.startAnimating()
+        startTimer()
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("Screen was touched.")
+    func itemDroppedOnCharacter(nitif: AnyObject) {
+        print("Item dropped on character.")
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            timer.invalidate()
+        }
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: "changeGameState", userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        penalties++
+        
+        if penalties == 1 {
+            imgLeftSkull.alpha = OPAQUE_ALPHA
+            imgCenterSkull.alpha = DIM_ALPHA
+        } else if penalties == 2 {
+            imgCenterSkull.alpha = OPAQUE_ALPHA
+            imgRightSkull.alpha = DIM_ALPHA
+        } else if penalties >= 3 {
+            imgRightSkull.alpha = OPAQUE_ALPHA
+        } else {
+            imgLeftSkull.alpha = DIM_ALPHA
+            imgCenterSkull.alpha = DIM_ALPHA
+            imgRightSkull.alpha = DIM_ALPHA
+        }
+        
+        if penalties >= MAX_PENALTIES {
+            gameOver()
+        }
+    }
+    
+    func gameOver() {
+        timer.invalidate()
+        imgMonster.playDeathAnimation()
     }
 
     override func didReceiveMemoryWarning() {
